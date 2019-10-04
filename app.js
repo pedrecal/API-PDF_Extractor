35279da8f2c148851e16c46661d764d198029e9b
Alexandre Pedrecal <pedrecalc@gmail.com>
2019-10-04 19:05:50 -0300

    :tada: Initial commit. TCC BEGINS

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
+
+const app = express();
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
+
+// After allllll that above middleware, we finally handle our own routes!
+app.use('/', routes);
+
+// done! we export it so we can start the site in start.js
+module.exports = app;
