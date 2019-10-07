1fc248f5123c00b86c65ceba1782199bafa30f03
Alexandre Pedrecal <pedrecalc@gmail.com>
2019-10-07 20:26:32 -0300

    :construction: add some routes (register and login)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Hey! It works!');
+});
+
+router.post('/register', userController.registerUser);
+
+router.post('/login', (req, res) => {
+  res.send('Register');
 });
 
 module.exports = router;
