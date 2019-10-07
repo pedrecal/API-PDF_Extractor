04ef9a7eefa83af2edb6f76ae8e3a485260e84c1
Alexandre Pedrecal <pedrecalc@gmail.com>
2019-10-07 20:25:20 -0300

    :card_file_box: add db and import User schema

--- start.js
+++ start.js
@@ -3,19 +3,21 @@ const mongoose = require('mongoose');
 // import environmental variables from our variables.env file
 require('dotenv').config({ path: 'variables.env' });
 
+mongoose.set('useCreateIndex', true);
 // Connect to our Database and handle any bad connections
-// mongoose.connect(process.env.DATABASE);
-// mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-// mongoose.connection.on('error', err => {
-//   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
-// });
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.on('error', err => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
 
 // READY?! Let's go!
 
 // import all of our models
-// require('./models/Store');
-// require('./models/User');
-// require('./models/Review');
+require('./models/User');
 
 // Start our app!
 const app = require('./app');
