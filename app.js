de8383107ee756f1178a3cf8e02d5dc911e7c672
Alexandre Pedrecal <pedrecalc@gmail.com>
2019-10-22 09:06:38 -0300

    :construction: add swagger basic config

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
 const routes = require('./routes/index');
 
 const app = express();
@@ -7,6 +13,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Exposes a bunch of methods for validating data. Used heavily on userController.validateRegister
 
+const packageInfo = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, 'package.json'), 'utf8')
+);
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      title: packageInfo.name,
+      version: packageInfo.version,
+      description: packageInfo.description,
+    },
+    basePath: `/api/`,
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJSDoc(options);
+app.use(`/api/docs`, swaggerUi.serve, swaggerUi.setup(specs));
 // After allllll that above middleware, we finally handle our own routes!
 app.use('/api', routes);
 
